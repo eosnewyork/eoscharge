eece1abeff8906d3872cfb1b4e9e8b93a010c6c5
Buddy Deck <deck@eosnewyork.io>
2018-08-28 11:26:43 -0400

    add arg for limit

--- scripts/mongo_trx_rsrc_avg.sh
+++ scripts/mongo_trx_rsrc_avg.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-echo "aggregating data for previous 100000 blocks"
-mongo EOS --eval 'db.blocks.aggregate([{"$sort":{"block_num":-1}},{"$limit":100000},{"$unwind":{"path":"$block.transactions","preserveNullAndEmptyArrays":false}},{"$unwind":{"path":"$block.transactions.trx.transaction.actions","preserveNullAndEmptyArrays":false}},{"$project":{"cpu_usage_us":"$block.transactions.cpu_usage_us","net_usage_words":"$block.transactions.net_usage_words","action_acct":"$block.transactions.trx.transaction.actions.account","action_name":"$block.transactions.trx.transaction.actions.name"}},{"$group":{"_id":{"acct":"$action_acct","name":"$action_name"},"avg_cpu_us":{"$avg":"$cpu_usage_us"},"avg_net_words":{"$avg":"$net_usage_words"},"count":{"$sum":1}}},{"$out":"trx_rsrc_avg_results_100000"}])'
+limit_num=100000
+
+if  [ $# -eq 1 ]; then
+   limit_num=$1
+fi
+
+echo "aggregating data for previous $limit_num blocks"
+mongo EOS --eval 'db.blocks.aggregate([{"$sort":{"block_num":-1}},{"$limit":'"$limit_num"'},{"$unwind":{"path":"$block.transactions","preserveNullAndEmptyArrays":false}},{"$unwind":{"path":"$block.transactions.trx.transaction.actions","preserveNullAndEmptyArrays":false}},{"$project":{"cpu_usage_us":"$block.transactions.cpu_usage_us","net_usage_words":"$block.transactions.net_usage_words","action_acct":"$block.transactions.trx.transaction.actions.account","action_name":"$block.transactions.trx.transaction.actions.name"}},{"$group":{"_id":{"acct":"$action_acct","name":"$action_name"},"avg_cpu_us":{"$avg":"$cpu_usage_us"},"avg_net_words":{"$avg":"$net_usage_words"},"count":{"$sum":1}}},{"$out":"trx_rsrc_avg_results_100000"}])'
 
 echo "aggregation done.  exporting to csv and json."
 mongoexport -d EOS -c trx_rsrc_avg_results_100000 -f _id.acct,_id.name,avg_cpu_us,avg_net_words,count --type=csv > trx_rsrc_avg_results_100000.csv
