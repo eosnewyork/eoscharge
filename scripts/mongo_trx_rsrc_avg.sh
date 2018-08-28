0557f4e54b59f41517bc371b4ca0b114d1e70de8
Buddy Deck <deck@eosnewyork.io>
2018-08-28 12:30:56 -0400

    add file_name option

--- scripts/mongo_trx_rsrc_avg.sh
+++ scripts/mongo_trx_rsrc_avg.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 limit_num=100000
+file_name=trx_rsrc_avg_results_$limit_num
 
 if  [ $# -eq 1 ]; then
    limit_num=$1
+elif [ $# -eq 2 ]; then
+   limit_num=$1
+   file_name=$2
 fi
 
 # get script dir
@@ -13,5 +17,5 @@ echo "aggregating data for previous $limit_num blocks"
 mongo EOS --eval 'db.blocks.aggregate([{"$sort":{"block_num":-1}},{"$limit":'"$limit_num"'},{"$unwind":{"path":"$block.transactions","preserveNullAndEmptyArrays":false}},{"$unwind":{"path":"$block.transactions.trx.transaction.actions","preserveNullAndEmptyArrays":false}},{"$project":{"cpu_usage_us":"$block.transactions.cpu_usage_us","net_usage_words":"$block.transactions.net_usage_words","action_acct":"$block.transactions.trx.transaction.actions.account","action_name":"$block.transactions.trx.transaction.actions.name"}},{"$group":{"_id":{"acct":"$action_acct","name":"$action_name"},"avg_cpu_us":{"$avg":"$cpu_usage_us"},"avg_net_words":{"$avg":"$net_usage_words"},"count":{"$sum":1}}},{"$out":"trx_rsrc_avg_results_100000"}])'
 
 echo "aggregation done.  exporting to csv and json."
-mongoexport -d EOS -c trx_rsrc_avg_results_$limit_num -f _id.acct,_id.name,avg_cpu_us,avg_net_words,count --type=csv > $DIR/trx_rsrc_avg_results_$limit_num.csv
-mongoexport -d EOS -c trx_rsrc_avg_results_$limit_num -f _id.acct,_id.name,avg_cpu_us,avg_net_words,count --type=json > $DIR/trx_rsrc_avg_results_$limit_num.json
+mongoexport -d EOS -c trx_rsrc_avg_results_$limit_num -f _id.acct,_id.name,avg_cpu_us,avg_net_words,count --type=csv > $DIR/$file_name.csv
+mongoexport -d EOS -c trx_rsrc_avg_results_$limit_num -f _id.acct,_id.name,avg_cpu_us,avg_net_words,count --type=json > $DIR/$file_name.json
