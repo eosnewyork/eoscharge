93070068f96310bba9f487c39442a408fe09e78d
Mike H <ghoti143@gmail.com>
2018-10-16 14:06:32 -0400

    adding hyperlink to title

--- src/components/ActionText.js
+++ src/components/ActionText.js
@@ -5,18 +5,27 @@ import Typography from '@material-ui/core/Typography'
 const styles = theme => ({
   description: {
     marginTop: theme.spacing.unit * 2
+  },
+  title: {
+    '& a': {
+      color: theme.palette.text.primary,
+      textDecoration: 'none',
+      '&:hover': {
+        textDecoration: 'underline'
+      }
+    }
   }
 })
 
 class ActionText extends Component {
 
   render() {
-    const {title, subtitle, topText, bottomText, classes} = this.props
+    const {acctName, title, subtitle, topText, bottomText, classes} = this.props
     
     return (
       <React.Fragment>
-        <Typography variant="display1" color="textPrimary">
-          {title}
+        <Typography variant="display1" color="textPrimary" className={classes.title}>
+          <a target="_blank" rel="noopener noreferrer" href="https://bloks.io/account/{acctName}">{title}</a>
         </Typography>
         <Typography variant="title" color="textSecondary">
           {subtitle}
