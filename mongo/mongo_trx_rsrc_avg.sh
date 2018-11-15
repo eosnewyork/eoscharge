22707542138a4e8a90d079c677a0d5fc90b36479
Buddy Deck <deck@eosnewyork.io>
2018-11-15 09:21:52 -0500

    revert to use blocks collection

--- mongo/mongo_trx_rsrc_avg.sh
+++ mongo/mongo_trx_rsrc_avg.sh
@@ -15,7 +15,7 @@ fi
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 echo "aggregating data for previous $limit_num blocks"
-mongo EOS --eval 'db.transaction_traces.aggregate([ {$sort: {_id: -1} }, {$limit: '"$limit_num"' }, {$unwind: {path: "$action_traces",preserveNullAndEmptyArrays: false} }, {$project: {cpu_usage_us: "$receipt.cpu_usage_us",net_usage_words: "$receipt.net_usage_words",action_acct: "$action_traces.act.account",action_name: "$action_traces.act.name"} }, { "$group": {"_id": {"acct": "$action_acct","name": "$action_name"},"avg_cpu_us": {"$avg": "$cpu_usage_us"},"avg_net_words": {"$avg": "$net_usage_words"},"count": {"$sum": 1} } }, { "$out": "trx_rsrc_avg_results"} ])'
+mongo EOS --eval 'db.blocks.aggregate([{"$sort":{"block_num":-1}},{"$limit":'"$limit_num"'},{"$unwind":{"path":"$block.transactions","preserveNullAndEmptyArrays":false}},{"$unwind":{"path":"$block.transactions.trx.transaction.actions","preserveNullAndEmptyArrays":false}},{"$project":{"cpu_usage_us":"$block.transactions.cpu_usage_us","net_usage_words":"$block.transactions.net_usage_words","action_acct":"$block.transactions.trx.transaction.actions.account","action_name":"$block.transactions.trx.transaction.actions.name"}},{"$group":{"_id":{"acct":"$action_acct","name":"$action_name"},"avg_cpu_us":{"$avg":"$cpu_usage_us"},"avg_net_words":{"$avg":"$net_usage_words"},"count":{"$sum":1}}},{"$out":"trx_rsrc_avg_results"}])'
 
 echo "aggregation done.  exporting to csv and json."
 mongoexport -d EOS -c trx_rsrc_avg_results -f _id.acct,_id.name,avg_cpu_us,avg_net_words,count --type=csv > $file_name.csv
