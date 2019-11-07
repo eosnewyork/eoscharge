efbcc3516bb7d300be162b5c73b49213a4710865
Buddy Deck <deck@eosnewyork.io>
2019-11-07 11:52:19 -0500

    add under construction

--- src/containers/Account.js
+++ src/containers/Account.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react'
-import { Provider } from "mobx-react"
-import AccountStore from '../stores/AccountStore'
-import AccountForm from './AccountForm'
-import AccountResources from './AccountResources'
-import withStyles from '@material-ui/core/styles/withStyles'
-import Typography from '@material-ui/core/Typography'
-import { withNamespaces } from 'react-i18next'
+import React, { Component } from "react";
+import { Provider } from "mobx-react";
+import AccountStore from "../stores/AccountStore";
+import AccountForm from "./AccountForm";
+import AccountResources from "./AccountResources";
+import withStyles from "@material-ui/core/styles/withStyles";
+import Typography from "@material-ui/core/Typography";
+import { withNamespaces } from "react-i18next";
 
 const styles = theme => ({
   heroButtons: {
@@ -14,20 +14,39 @@ const styles = theme => ({
   acctWrapper: {
     marginBottom: theme.spacing.unit * 4
   }
-})
+});
 
-class Account extends Component {  
-
+class Account extends Component {
   render() {
-    const {classes, t} = this.props
+    const { classes, t } = this.props;
 
     return (
       <div className={classes.acctWrapper}>
-        <Typography variant="display3" align="center" color="textPrimary" gutterBottom>
-          {t('TAKE_CHARGE')}
+        <Typography
+          variant="display3"
+          align="center"
+          color="yellow"
+          gutterBottom
+        >
+          {t("UNDER_CONSTRUCTION")}
         </Typography>
-        <Typography variant="title" align="center" color="textSecondary" paragraph>
-          {t('ENTER_YOUR_ACCOUNT', {currency: this.props.network.toUpperCase()})}
+        <Typography
+          variant="display3"
+          align="center"
+          color="textPrimary"
+          gutterBottom
+        >
+          {t("TAKE_CHARGE")}
+        </Typography>
+        <Typography
+          variant="title"
+          align="center"
+          color="textSecondary"
+          paragraph
+        >
+          {t("ENTER_YOUR_ACCOUNT", {
+            currency: this.props.network.toUpperCase()
+          })}
         </Typography>
         <div className={classes.heroButtons}>
           <Provider store={AccountStore}>
@@ -38,13 +57,8 @@ class Account extends Component {
           </Provider>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default 
-withNamespaces()(
-  withStyles(styles)(
-    Account
-  )
-)
+export default withNamespaces()(withStyles(styles)(Account));
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,97 +1,98 @@
-import React, { Component } from 'react'
-import CssBaseline from '@material-ui/core/CssBaseline'
-import Header from './Header'
-import Footer from './Footer'
-import Home from './Home'
-import Faq from './Faq'
-import Disclaimer from './Disclaimer'
-import Links from './Links'
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
-import { Route } from 'react-router-dom'
-import { WWW_STR, EOS_NETWORK } from '../config.js'
+import React, { Component } from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Header from "./Header";
+import Footer from "./Footer";
+import Home from "./Home";
+import Faq from "./Faq";
+import Disclaimer from "./Disclaimer";
+import Links from "./Links";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import { Route } from "react-router-dom";
+import { WWW_STR, EOS_NETWORK } from "../config.js";
 
 const theme = createMuiTheme({
   typography: {
-    fontFamily: [
-      '"Barlow Semi Condensed"',
-      'sans-serif'
-    ].join(','),
+    fontFamily: ['"Barlow Semi Condensed"', "sans-serif"].join(",")
   },
   palette: {
     primary: {
-      light: '#A4BDC3',
-      main: '#1B5A6C',
-      dark: '#3a93a5'
+      light: "#A4BDC3",
+      main: "#1B5A6C",
+      dark: "#3a93a5"
     },
     secondary: {
-      main: '#A4BDC3'
+      main: "#A4BDC3"
     },
     text: {
-      primary: '#323233',
-      secondary: '#7B7D7D'
+      primary: "#323233",
+      secondary: "#7B7D7D"
     },
     background: {
-      paper: '#FFFFFF',
-      default: '#EDEDED'
+      paper: "#FFFFFF",
+      default: "#EDEDED"
     }
   }
-})
+});
 
 class App extends Component {
-  
   constructor(props) {
     super(props);
     let net = EOS_NETWORK;
 
     let hostName = window.location.host.toLowerCase().replace(WWW_STR, "");
     let subdomain = hostName.split(".")[0];
-    console.info(subdomain)
-    if(! subdomain.startsWith(net) && ! subdomain.startsWith("localhost")) {
+    console.info(subdomain);
+    if (!subdomain.startsWith(net) && !subdomain.startsWith("localhost")) {
       net = subdomain;
     }
     this.state = {
-      network: net,
+      network: net
     };
     this.handleNetworkChange = this.handleNetworkChange.bind(this);
   }
 
   handleNetworkChange(network) {
     let urlPrefix = window.location.protocol + "//" + WWW_STR;
-    // remove www. from host 
-    let hostName = window.location.host.toLowerCase().replace(WWW_STR, ""); 
-    let urlSuffix = hostName + window.location.pathname + window.location.search;
+    // remove www. from host
+    let hostName = window.location.host.toLowerCase().replace(WWW_STR, "");
+    let urlSuffix =
+      hostName + window.location.pathname + window.location.search;
     let url = urlPrefix + urlSuffix;
-    if ( network !== EOS_NETWORK) {
+    if (network !== EOS_NETWORK) {
       url = urlPrefix + network + "." + urlSuffix;
-    } 
-    else {
+    } else {
       // remove subdomain if network is eos
-      url = url.replace(this.state.network + ".", "")
+      url = url.replace(this.state.network + ".", "");
     }
     this.setState({
-      network: network,
+      network: network
     });
     window.location.replace(url);
   }
 
   render() {
-
-    document.title = `${this.state.network.toUpperCase()} Charge`
+    document.title = `${this.state.network.toUpperCase()} Charge (Under Construction)`;
     return (
       <MuiThemeProvider theme={theme}>
-        <CssBaseline />            
-        <Header handleNetworkChange={this.handleNetworkChange} network={this.state.network}/>
-        <main>        
-          <Route path="/" exact 
-                 render={(props) => <Home {...props} network={this.state.network} />} />
+        <CssBaseline />
+        <Header
+          handleNetworkChange={this.handleNetworkChange}
+          network={this.state.network}
+        />
+        <main>
+          <Route
+            path="/"
+            exact
+            render={props => <Home {...props} network={this.state.network} />}
+          />
           <Route path="/faq" component={Faq} />
           <Route path="/disclaimer" component={Disclaimer} />
           <Route path="/links" component={Links} />
         </main>
-        <Footer />      
+        <Footer />
       </MuiThemeProvider>
-    )
+    );
   }
 }
 
-export default App
+export default App;
